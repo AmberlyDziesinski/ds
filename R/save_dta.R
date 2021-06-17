eecebe57ef3406f7258a9acc778cf3193a88c633
AmberlyDziesinski <31117075+AmberlyDziesinski@users.noreply.github.com>
2021-06-17 11:45:29 -0500

    updates
    not done with tutorial yet

--- R/save_dta.R
+++ R/save_dta.R
@@ -4,12 +4,12 @@
 #
 #
 # library(tidyverse)
-# library(haven)
+#library(haven)
 #
 #library(devtools)
 #use_mit_license("Amberly Dziesinski")
 
-
+#stata files
 # vote_gov_states <- read_dta(file = "C:\\Users\\amber\\Documents\\data_sci\\vote_data\\vote_gov_states.dta")
 # vote_pres_national <- read_dta(file = "C:\\Users\\amber\\Documents\\data_sci\\vote_data\\vote_pres_national.dta")
 # vote_pres_primaries <- read_dta(file = "C:\\Users\\amber\\Documents\\data_sci\\vote_data\\vote_pres_primaries.dta")
@@ -22,5 +22,12 @@
 # usethis::use_data(vote_pres_states, overwrite = TRUE)
 # usethis::use_data(vote_sen_states, overwrite = TRUE)
 
-# hsls <- load(file = "C:\\Users\\amber\\Documents\\data_sci\\hsls_data\\hsls_clean.rda")
+##R files
+#hsls <- load(file = "C:\\Users\\amber\\Documents\\data_sci\\hsls_data\\hsls.rda")
 # usethis::use_data(hsls, overwrite = TRUE)
+
+
+##MAKE TUTORIALS
+#library(learnr)
+#usethis::use_tutorial("conditional_means", "Conditional Means Tutorial", open = interactive())
+
